95bb7093452d3f5be1983f5e157a3d35d9b89b38
rafiq9190 <muhammadrafiq9190@gmail.com>
2020-06-09 17:56:32 +0500

    error update

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,8 @@ export default function Home (props){
     return(
 <div >
     <p>helo from home page</p>
-<h2>my name is {props.first-Name}</h2>
-<h3>my father is{props.last-name}</h3>
+<h2>my name is {props.firstName}</h2>
+<h3>my father is{props.lastName}</h3>
     <h4>this color is {props.color}</h4>
 </div>
 
