471a8ebebe8230006451d5fa5f35f2192ccffec4
rafiq9190 <muhammadrafiq9190@gmail.com>
2020-06-09 17:41:49 +0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import Home from './components/home'
 
 import './App.css';
 
- function App(props) {
+ function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
       <h2>we are learn now things </h2>
-      <Home/>
-      <h2>{props.fName}</h2>
-        <h3>{props.lName}</h3>
+      <Home  fname="raifq" lName="hanif"/>
+    
        
       
     </div>
