ab211771d5e3e6c32ed95e9ce4925e39e9b66931
rafiq9190 <muhammadrafiq9190@gmail.com>
2020-06-09 16:52:09 +0500

    custome component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Home from './components/home'
 
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <h1>hello world</h1>
       <h2>we are learn now things </h2>
+      {props.color};
+      {props.price}
     </div>
   );
 }
---
+++ src/components/home.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+function Home (){
+<div className="color:black; price:20000k">helo from home page</div>
+}
